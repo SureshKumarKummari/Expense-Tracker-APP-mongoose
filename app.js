c15c3c2d8bef9128f0a1f5bac20034adbcd6b0dc
SureshKumarKummari <sureshk28591@gmail.com>
2024-04-05 11:18:53 +0530

    Signup and Login Form

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+const express = require('express');
+
+const cors=require('cors');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+
+
+app.use(bodyParser.json());
+
+
+
+app.listen(3000,()=>{
+    console.log("App listening on PORT 3000!")
+})
+
