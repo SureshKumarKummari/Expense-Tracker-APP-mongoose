68562f6bfbc32199f9d5d15bd82aa654561e2f25
SureshKumarKummari <sureshk28591@gmail.com>
2024-04-05 16:48:57 +0530

    Login Backend

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,7 +2,7 @@ const users=require('../models/users');
 
 
 exports.signup=(req,res,next)=>{
-    users.findByPk(req.params.email)
+    users.findOne({where:{email:req.body.email}})
     .then(user=>{
         if(!user){
             return users.create({
@@ -11,12 +11,12 @@ exports.signup=(req,res,next)=>{
                     password:req.body.password
             })
         }else{
-            res.status(409).json({error:'User already exists!'})
+            res.status(404).json({error:'User already exists!'})
             //res.status(409).send({error:'User already exists!'});  14 & 15 both are same Just 14 will sends JSON only while 15 will all types of responses like html JSON plain text
 
         }
     }).then(user=>{
-        res.send(user.dataValues);
+        res.send(user);
     }).catch(err=>console.log(err));
 }
 
@@ -24,8 +24,12 @@ exports.signup=(req,res,next)=>{
 exports.login=(req,res,next)=>{
     users.findOne({where :{email: req.params.email}})
     .then(user=>{
-        console.log(user.dataValues);
+        if(user && user.password===req.params.password){
+        console.log(user);
         res.send(user);
+        }else{
+            res.status(404).json({error:'Invalid Credentials!'})
+        }
     }).catch(err=>console.log(err));    
 
 }
